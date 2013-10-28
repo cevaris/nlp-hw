592a2b1df9bab7ea8a5ceae33ca1e94ac215d165
James Kobol <jim.kobol@gmail.com>
2013-10-28 11:03:14 -0600

    got printout of bigrams and their respected tags

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -5,14 +5,20 @@ import re
 # INPUT_FILE = '10fitness.txt'
 INPUT_FILE = 'test.txt'
 
+BEGIN_OF_LINE = 'BOL'
 END_OF_LINE = 'EOL'
 
 
 
 def from_file( file_name ):
-	token_counts = {}
-
 	assert file_name, "File Input not provided"
+
+	tag_trans = {}
+	word_likelihoods = {}
+
+
+	prev_tup = (None, None)
+
 	delim = ['.','.']
 	
 	with open(file_name) as f:
@@ -20,14 +26,23 @@ def from_file( file_name ):
 			# print "%s" % line.split()
 			items = line.split()
 			
-			if (items == ['.','.']):
-				print END_OF_LINE
-			elif (items == []):
+			if (items == []):
 				continue
 			else:
+
 				token, tag = tuple(items)
-				print "%s %s" % (token, tag)
-		
+
+				if (items == delim):
+					print END_OF_LINE
+					prev_tup = (None, None)
+
+				elif (prev_tup == (None,None)):
+					print BEGIN_OF_LINE
+					prev_tup = (token, tag)
+				else:
+					prev_token, prev_tag = prev_tup
+					print "%s|%s, %s|%s" % (prev_token, prev_tag, token, tag)
+					prev_tup = (token, tag)
 
 		
 def main():
